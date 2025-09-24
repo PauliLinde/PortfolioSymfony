2d98910c17cec62188b7016458da4047a44a5915
PauliLinde <jus.paulina@gmail.com>
2025-09-24 17:56:30 +0200

    Made changes in controller class to save message in db and fix cors problem

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,28 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\Message;
-use App\Form\Type\MessageType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MessageController extends AbstractController
 {
 
-    #[Route('/newMessage', methods: ['POST'])]
-    public function newMessage(Request $request, EntityManagerInterface $entityManager): void
+    #[Route('/newMessage', methods: ['POST', 'OPTIONS'])]
+    public function newMessage(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
+        if ($request->getMethod() === 'OPTIONS') {
+            return new JsonResponse(null, 200, [
+                'Access-Control-Allow-Origin' => 'http://localhost:5174', // SvelteKit dev server
+                'Access-Control-Allow-Methods' => 'POST, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type',
+            ]);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if(!$data){
+            return new JsonResponse(['error' => 'Invalid data'], 400);
+        }
 
         $message = new Message();
+        $message->setEmail($data['email']);
+        $message->setMessage($data['message']);
 
-        $form = $this->createForm(MessageType::class, $message);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $action = $form->getData();
-            $entityManager->persist($action);
-            $entityManager->flush();
-        }
+        $entityManager->persist($message);
+        $entityManager->flush();
+        return new JsonResponse(['success' => true, 'id' => $message->getId()]);
     }
 }
