9a1966b11d390bbc510c96cdb32f269a34aa4868
PauliLinde <jus.paulina@gmail.com>
2025-09-25 11:19:15 +0200

    Adding some files to github

---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    require dirname(__DIR__).'/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
+if (file_exists(dirname(__DIR__).'/.env.test') && (!isset($_ENV['APP_ENV']) || $_ENV['APP_ENV'] === 'test')) {
+    (new Dotenv())->load(dirname(__DIR__).'/.env.test');
+}
+
+if ($_SERVER['APP_DEBUG'] ?? false) {
+    umask(0000);
+}
