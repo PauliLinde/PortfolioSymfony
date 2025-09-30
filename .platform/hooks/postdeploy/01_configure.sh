85b04ff612562afb748748ff9eec6bbba8f0b362
PauliLinde <jus.paulina@gmail.com>
2025-09-30 14:40:07 +0200

    Move index.php to root for Elastic Beanstalk

--- .platform/hooks/postdeploy/01_configure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ensure proper permissions
-chmod -R 755 /var/app/current/public
-chown -R webapp:webapp /var/app/current/var
-
-# Clear Symfony cache
-cd /var/app/current
-php bin/console cache:clear --no-warmup --env=prod
-php bin/console cache:warmup --env=prod
